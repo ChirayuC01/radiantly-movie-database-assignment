0ef039ff0bc17423fc939d352cfb229fd47a9c1c
Chriayu01 <chirayuchawande01@gmail.com>
2025-01-14 18:50:03 +0530

    assignment completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GenreMovieList from "./Components/GenreMovieList";
+
+function App() {
+  return (
+    <>
+      <GenreMovieList />
+    </>
+  );
+}
+
+export default App;
